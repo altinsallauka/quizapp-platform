e947eb3b942b92de5357e67dac7510a79dee1fc2
Altin Sallauka <sallaukaaltin@gmail.com>
2021-05-26 18:24:22 +0300

    ...

--- client/src/components/role-page/RolePage.jsx
+++ client/src/components/role-page/RolePage.jsx
@@ -7,14 +7,16 @@ export default class RolePage extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="container">
-          <h1>RolePage</h1>
-          <Link to={"/#"} className="nav-link">
-            Student
-          </Link>
-          <Link to={"/login"} className="nav-link">
-            Mentor
-          </Link>
+        <div className="container role-content">
+          <h1 className="mb-5">Start using our platform as</h1>
+          <div className="role-links">
+            <Link to={"/#"} className="nav-link">
+              Student
+            </Link>
+            <Link to={"/login"} className="nav-link">
+              Mentor
+            </Link>
+          </div>
         </div>
       </div>
     );
