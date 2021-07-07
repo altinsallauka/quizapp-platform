cd667153b2868868ba70503ff7f373c5e4f75246
Altin Sallauka <sallaukaaltin@gmail.com>
2021-07-07 21:17:52 +0200

    added jquery & bootstrap npm, fixed navbar toggle on mobile

--- client/src/components/dashboard/nav-bar/NavBar.jsx
+++ client/src/components/dashboard/nav-bar/NavBar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./NavBar.scss";
 import { Link } from "react-router-dom";
+// import "../node_modules/jquery/dist/jquery.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import "../../../../node_modules/jquery/dist/jquery.min.js";
+import "../../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 export default class NavBar extends React.Component {
   state = {};
   logOut() {
@@ -9,14 +13,24 @@ export default class NavBar extends React.Component {
   }
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-primary">
-          <div className="container">
-            <a href="/home" className="navbar-brand">
-              Quizapp MERN
-            </a>
-            {/* <div className="navbar-nav mr-auto"> */}
-            <div className="navbar-nav">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container">
+          <a href="/home" className="navbar-brand">
+            Quizapp MERN
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
               <li className="nav-item">
                 <Link to={"/questions"} className="nav-link">
                   Questions
@@ -46,10 +60,51 @@ export default class NavBar extends React.Component {
                   Log out
                 </Link>
               </li>
-            </div>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
+      // <div>
+      //   <nav className="navbar navbar-expand navbar-dark bg-primary">
+      //     <div className="container">
+      //       <a href="/home" className="navbar-brand">
+      //         Quizapp MERN
+      //       </a>
+      //       {/* <div className="navbar-nav mr-auto"> */}
+      //       <div className="navbar-nav">
+      //         <li className="nav-item">
+      //           <Link to={"/questions"} className="nav-link">
+      //             Questions
+      //           </Link>
+      //         </li>
+      //         <li className="nav-item">
+      //           <Link to={"/categories"} className="nav-link">
+      //             Categories
+      //           </Link>
+      //         </li>
+      //         <li className="nav-item">
+      //           <Link to={"/users"} className="nav-link">
+      //             Users
+      //           </Link>
+      //         </li>
+      //         <li className="nav-item">
+      //           <Link to={"/my-profile"} className="nav-link">
+      //             My Profile
+      //           </Link>
+      //         </li>
+      //         <li className="nav-item">
+      //           <Link
+      //             to={"/role"}
+      //             className="nav-link"
+      //             onClick={() => this.logOut()}
+      //           >
+      //             Log out
+      //           </Link>
+      //         </li>
+      //       </div>
+      //     </div>
+      //   </nav>
+      // </div>
     );
   }
 }
