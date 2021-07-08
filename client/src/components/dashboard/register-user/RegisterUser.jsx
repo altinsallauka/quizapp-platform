7d1900f90309eb6c59082964380e8075313ad658
Altin Sallauka <sallaukaaltin@gmail.com>
2021-07-08 15:18:04 +0200

    Added role when user is created.

--- client/src/components/dashboard/register-user/RegisterUser.jsx
+++ client/src/components/dashboard/register-user/RegisterUser.jsx
@@ -9,30 +9,38 @@ export default class RegisterUser extends React.Component {
       lastName: "",
       username: "",
       password: "",
+      roleId: "",
+      roles: [],
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
   async handleSubmit(event) {
     event.preventDefault();
-    const { firstName, lastName, username, password } = this.state;
+    const { firstName, lastName, username, roleId, password } = this.state;
     const { data } = await axios.post("http://localhost:3001/users/register", {
       firstName,
       lastName,
+      roleId,
       username,
       password,
     });
     console.log(data.data);
     this.props.history.push("/users");
   }
-
-  //   componentDidMount() {}
+  getRoles() {
+    axios.get("http://localhost:3001/roles").then((res) => {
+      this.setState({ roles: res.data });
+    });
+  }
+  componentDidMount() {
+    this.getRoles();
+  }
   render() {
     return (
       <div className="row">
@@ -75,6 +83,26 @@ export default class RegisterUser extends React.Component {
                 onChange={this.handleChange}
               />
             </label>
+            <div className="d-flex align-items-center">
+              <label className="d-flex flex-column align-items-start">
+                Role:
+                <select
+                  className="form-select"
+                  name="roleId"
+                  aria-label="Default select example"
+                  onChange={this.handleChange}
+                >
+                  <option disabled selected>
+                    Select one role
+                  </option>
+                  {this.state.roles.map((userRole) => (
+                    <option value={userRole._id} key={userRole._id}>
+                      {userRole.role}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
             <input
               type="submit"
               className="btn btn-primary mt-2"
