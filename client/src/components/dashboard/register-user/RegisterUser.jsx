0a8a3b5e1a76dfed519a8c5f8568e8103fcebd70
Altin Sallauka <sallaukaaltin@gmail.com>
2021-10-03 14:31:29 +0200

    reg user to sfc

--- client/src/components/dashboard/register-user/RegisterUser.jsx
+++ client/src/components/dashboard/register-user/RegisterUser.jsx
@@ -1,31 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./RegisterUser.scss";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-export default class RegisterUser extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      access_token: localStorage.getItem("token"),
-      firstName: "",
-      lastName: "",
-      username: "",
-      password: "",
-      roleId: "",
-      roles: [],
-    };
+const RegisterUser = (props) => {
+  const [access_token, setAccessToken] = useState(
+    localStorage.getItem("token")
+  );
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [roleId, setRoleId] = useState("");
+  const [roles, setRoles] = useState([]);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  async handleSubmit(event) {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { firstName, lastName, username, roleId, password } = this.state;
+    // const { firstName, lastName, username, roleId, password } = this.state;
     await axios
       .post("http://localhost:3001/users/register", {
         firstName,
@@ -35,96 +26,98 @@ export default class RegisterUser extends React.Component {
         password,
       })
       .then((res) => {
-        this.props.history.push("/users");
+        props.history.push("/users");
         toast.success("User has been registered!");
       })
       .catch((err) => {
         toast.error(err.response.data.message);
       });
-  }
-  getRoles() {
+  };
+  const getRoles = () => {
     axios
       .get("http://localhost:3001/roles")
       .then((res) => {
-        this.setState({ roles: res.data });
+        // this.setState({ roles: res.data });
+        setRoles(res.data);
       })
       .catch((err) => {
         toast.error(err.response.data.message);
       });
-  }
-  componentDidMount() {
-    this.getRoles();
-  }
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-6">
-          <h1>Register User</h1>
-          <form onSubmit={this.handleSubmit}>
-            <label className="d-flex flex-column align-items-start">
-              First Name:
-              <input
-                type="text"
-                name="firstName"
-                className="form-control"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label className="d-flex flex-column align-items-start">
-              Last Name:
-              <input
-                type="text"
-                name="lastName"
-                className="form-control"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label className="d-flex flex-column align-items-start">
-              Username:
-              <input
-                type="text"
-                name="username"
-                className="form-control"
-                onChange={this.handleChange}
-              />
-            </label>
-            <label className="d-flex flex-column align-items-start">
-              Password:
-              <input
-                type="password"
-                name="password"
-                className="form-control"
-                onChange={this.handleChange}
-              />
-            </label>
-            <div className="d-flex align-items-center">
-              <label className="d-flex flex-column align-items-start">
-                Role:
-                <select
-                  className="form-select"
-                  name="roleId"
-                  aria-label="Default select example"
-                  onChange={this.handleChange}
-                >
-                  <option disabled selected>
-                    Select one role
-                  </option>
-                  {this.state.roles.map((userRole) => (
-                    <option value={userRole._id} key={userRole._id}>
-                      {userRole.role}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
+  };
+  useEffect(() => {
+    getRoles();
+  }, []);
+
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <h1>Register User</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label className="d-flex flex-column align-items-start">
+            First Name:
             <input
-              type="submit"
-              className="btn btn-primary mt-2"
-              value="Submit"
+              type="text"
+              name="firstName"
+              className="form-control"
+              onChange={(e) => setFirstname(e.target.value)}
             />
-          </form>
-        </div>
+          </label>
+          <label className="d-flex flex-column align-items-start">
+            Last Name:
+            <input
+              type="text"
+              name="lastName"
+              className="form-control"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </label>
+          <label className="d-flex flex-column align-items-start">
+            Username:
+            <input
+              type="text"
+              name="username"
+              className="form-control"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label className="d-flex flex-column align-items-start">
+            Password:
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <div className="d-flex align-items-center">
+            <label className="d-flex flex-column align-items-start">
+              Role:
+              <select
+                className="form-select"
+                name="roleId"
+                aria-label="Default select example"
+                onChange={(e) => setRoleId(e.target.value)}
+              >
+                <option disabled selected>
+                  Select one role
+                </option>
+                {this.state.roles.map((userRole) => (
+                  <option value={userRole._id} key={userRole._id}>
+                    {userRole.role}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary mt-2"
+            value="Submit"
+          />
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default RegisterUser;
