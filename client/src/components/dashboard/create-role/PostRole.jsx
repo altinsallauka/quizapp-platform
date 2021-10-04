d50dd145e665dc3fd3f29a13cae45f971f273411
Altin Sallauka <sallaukaaltin@gmail.com>
2021-10-04 14:30:45 +0200

    myprofile, postrole to sfc

--- client/src/components/dashboard/create-role/PostRole.jsx
+++ client/src/components/dashboard/create-role/PostRole.jsx
@@ -1,21 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PostRole.scss";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-export default class PostRole extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { role: "", access_token: localStorage.getItem("token") };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-  async handleSubmit(event) {
+// export default class PostRole extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { role: "", access_token: localStorage.getItem("token") };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({ [event.target.name]: event.target.value });
+//   }
+//   async handleSubmit(event) {
+//     event.preventDefault();
+//     const { role } = this.state;
+//     await axios
+//       .post(
+//         "http://localhost:3001/roles",
+//         {
+//           role,
+//         },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${this.state.access_token}`,
+//           },
+//         }
+//       )
+//       .then((res) => {
+//         toast.success("New role has been added!");
+//         this.props.history.push("/users");
+//       })
+//       .catch((err) => {
+//         toast.error(err.response.data.message);
+//       });
+//   }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div className="row">
+//           <div className="container">
+//             <div className="col-md-6">
+//               <form onSubmit={this.handleSubmit}>
+//                 <div className="mb-3">
+//                   <h1>Create Role</h1>
+//                   <label for="roleNameInput" className="form-label">
+//                     Role name
+//                   </label>
+//                   <input
+//                     type="text"
+//                     name="role"
+//                     className="form-control"
+//                     id="roleNameInput"
+//                     onChange={this.handleChange}
+//                   />
+//                 </div>
+//                 <button type="submit" className="btn btn-primary">
+//                   Create
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+const PostRole = (props) => {
+  const [access_token, setAccess_token] = useState(
+    localStorage.getItem("token")
+  );
+  const [role, setRole] = useState("");
+
+  const handleChange = (event) => {
+    // this.setState({ [event.target.name]: event.target.value });
+    setRole(event.target.value);
+  };
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { role } = this.state;
+    // const { role } = this.state;
     await axios
       .post(
         "http://localhost:3001/roles",
@@ -24,46 +88,47 @@ export default class PostRole extends React.Component {
         },
         {
           headers: {
-            Authorization: `Bearer ${this.state.access_token}`,
+            Authorization: `Bearer ${access_token}`,
           },
         }
       )
       .then((res) => {
         toast.success("New role has been added!");
-        this.props.history.push("/users");
+        props.history.push("/roles");
       })
       .catch((err) => {
         toast.error(err.response.data.message);
       });
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <div className="row">
-          <div className="container">
-            <div className="col-md-6">
-              <form onSubmit={this.handleSubmit}>
-                <div className="mb-3">
-                  <h1>Create Role</h1>
-                  <label for="roleNameInput" className="form-label">
-                    Role name
-                  </label>
-                  <input
-                    type="text"
-                    name="role"
-                    className="form-control"
-                    id="roleNameInput"
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Create
-                </button>
-              </form>
-            </div>
+  };
+
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="container">
+          <div className="col-md-6">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <h1>Create Role</h1>
+                <label for="roleNameInput" className="form-label">
+                  Role name
+                </label>
+                <input
+                  type="text"
+                  name="role"
+                  className="form-control"
+                  id="roleNameInput"
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Create
+              </button>
+            </form>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default PostRole;
--- client/src/components/dashboard/my-profile/MyProfile.jsx
+++ client/src/components/dashboard/my-profile/MyProfile.jsx
@@ -1,132 +1,254 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./MyProfile.scss";
 import userImageSrc from "../../../assets/user.jpg";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-export default class MyProfile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      access_token: localStorage.getItem("token"),
-      user: [],
-    };
+// export default class MyProfile extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       access_token: localStorage.getItem("token"),
+//       user: [],
+//     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleChange(event) {
-    this.setState({
-      user: {
-        ...this.state.user,
-        [event.target.name]: event.target.value,
-      },
+//   handleChange(event) {
+//     this.setState({
+//       user: {
+//         ...this.state.user,
+//         [event.target.name]: event.target.value,
+//       },
+//     });
+//   }
+
+//   async handleSubmit(event) {
+//     event.preventDefault();
+//     const { user } = this.state;
+//     axios
+//       .put(`http://localhost:3001/users/${user.id}`, user, {
+//         headers: {
+//           Authorization: `Bearer ${this.state.access_token}`,
+//         },
+//       })
+//       .then((res) => {
+//         console.log("user", res.data);
+//         // this.setState({ user: res.data });
+//         this.props.history.push("/users");
+//         toast.success("Your profile has been updated!");
+//       })
+//       .catch((err) => {
+//         toast.error(err.response.data.message);
+//       });
+//   }
+//   getCurrentUser() {
+//     axios
+//       .get("http://localhost:3001/users/current", {
+//         headers: {
+//           Authorization: `Bearer ${this.state.access_token}`,
+//         },
+//       })
+//       .then((res) => {
+//         console.log(res.data);
+//         const user = res.data;
+//         this.setState({ user });
+//       })
+//       .catch((err) => {
+//         toast.error(err.response.data.message);
+//       });
+//   }
+//   componentDidMount() {
+//     this.getCurrentUser();
+//   }
+//   render() {
+//     return (
+//       <div className="row">
+//         <div className="col-md-6 mt-5">
+//           <h1>My Profile</h1>
+//           <hr />
+//           <div className="profile">
+//             <div>
+//               <img src={userImageSrc} alt="User profile" className="user-pic" />
+//             </div>
+//             <div>
+//               {/* <span>info</span> */}
+
+//               <form onSubmit={this.handleSubmit}>
+//                 <label className="d-flex flex-column align-items-start">
+//                   First Name:
+//                   <input
+//                     type="text"
+//                     name="firstName"
+//                     value={this.state.user.firstName}
+//                     className="form-control"
+//                     onChange={this.handleChange}
+//                   />
+//                 </label>
+//                 <label className="d-flex flex-column align-items-start">
+//                   Last Name:
+//                   <input
+//                     type="text"
+//                     name="lastName"
+//                     value={this.state.user.lastName}
+//                     className="form-control"
+//                     onChange={this.handleChange}
+//                   />
+//                 </label>
+//                 <label className="d-flex flex-column align-items-start">
+//                   Username:
+//                   <input
+//                     type="text"
+//                     name="username"
+//                     value={this.state.user.username}
+//                     className="form-control"
+//                     onChange={this.handleChange}
+//                   />
+//                 </label>
+//                 <label className="d-flex flex-column align-items-start">
+//                   New Password:
+//                   <input
+//                     type="password"
+//                     name="password"
+//                     value={this.state.user.password}
+//                     className="form-control"
+//                     onChange={this.handleChange}
+//                   />
+//                 </label>
+//                 <input
+//                   type="submit"
+//                   className="btn btn-primary mt-2"
+//                   value="Submit"
+//                 />
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const MyProfile = (props) => {
+  const [access_token, setAccess_token] = useState(
+    localStorage.getItem("token")
+  );
+  const [user, setUser] = useState([]);
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  async handleSubmit(event) {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { user } = this.state;
+    // const { user } = this.state;
     axios
       .put(`http://localhost:3001/users/${user.id}`, user, {
         headers: {
-          Authorization: `Bearer ${this.state.access_token}`,
+          Authorization: `Bearer ${access_token}`,
         },
       })
       .then((res) => {
         console.log("user", res.data);
         // this.setState({ user: res.data });
-        this.props.history.push("/users");
+        props.history.push("/users");
         toast.success("Your profile has been updated!");
       })
       .catch((err) => {
         toast.error(err.response.data.message);
       });
-  }
-  getCurrentUser() {
+  };
+  const getCurrentUser = () => {
     axios
       .get("http://localhost:3001/users/current", {
         headers: {
-          Authorization: `Bearer ${this.state.access_token}`,
+          Authorization: `Bearer ${access_token}`,
         },
       })
       .then((res) => {
         console.log(res.data);
         const user = res.data;
-        this.setState({ user });
+        // this.setState({ user });
+        setUser(user);
       })
       .catch((err) => {
         toast.error(err.response.data.message);
       });
-  }
-  componentDidMount() {
-    this.getCurrentUser();
-  }
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-6 mt-5">
-          <h1>My Profile</h1>
-          <hr />
-          <div className="profile">
-            <div>
-              <img src={userImageSrc} alt="User profile" className="user-pic" />
-            </div>
-            <div>
-              {/* <span>info</span> */}
+  };
 
-              <form onSubmit={this.handleSubmit}>
-                <label className="d-flex flex-column align-items-start">
-                  First Name:
-                  <input
-                    type="text"
-                    name="firstName"
-                    value={this.state.user.firstName}
-                    className="form-control"
-                    onChange={this.handleChange}
-                  />
-                </label>
-                <label className="d-flex flex-column align-items-start">
-                  Last Name:
-                  <input
-                    type="text"
-                    name="lastName"
-                    value={this.state.user.lastName}
-                    className="form-control"
-                    onChange={this.handleChange}
-                  />
-                </label>
-                <label className="d-flex flex-column align-items-start">
-                  Username:
-                  <input
-                    type="text"
-                    name="username"
-                    value={this.state.user.username}
-                    className="form-control"
-                    onChange={this.handleChange}
-                  />
-                </label>
-                <label className="d-flex flex-column align-items-start">
-                  New Password:
-                  <input
-                    type="password"
-                    name="password"
-                    value={this.state.user.password}
-                    className="form-control"
-                    onChange={this.handleChange}
-                  />
-                </label>
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  return (
+    <div className="row">
+      <div className="col-md-6 mt-5">
+        <h1>My Profile</h1>
+        <hr />
+        <div className="profile">
+          <div>
+            <img src={userImageSrc} alt="User profile" className="user-pic" />
+          </div>
+          <div>
+            {/* <span>info</span> */}
+
+            <form onSubmit={handleSubmit}>
+              <label className="d-flex flex-column align-items-start">
+                First Name:
                 <input
-                  type="submit"
-                  className="btn btn-primary mt-2"
-                  value="Submit"
+                  type="text"
+                  name="firstName"
+                  value={user.firstName}
+                  className="form-control"
+                  onChange={(e) => handleChange(e)}
                 />
-              </form>
-            </div>
+              </label>
+              <label className="d-flex flex-column align-items-start">
+                Last Name:
+                <input
+                  type="text"
+                  name="lastName"
+                  value={user.lastName}
+                  className="form-control"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+              <label className="d-flex flex-column align-items-start">
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  value={user.username}
+                  className="form-control"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+              <label className="d-flex flex-column align-items-start">
+                New Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={user.password}
+                  className="form-control"
+                  onChange={(e) => handleChange(e)}
+                />
+              </label>
+              <input
+                type="submit"
+                className="btn btn-primary mt-2"
+                value="Submit"
+              />
+            </form>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default MyProfile;
