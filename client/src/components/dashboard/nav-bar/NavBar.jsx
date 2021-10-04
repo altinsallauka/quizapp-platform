1849923017adc02ea1fce1204aec3f336659e1a6
Altin Sallauka <sallaukaaltin@gmail.com>
2021-10-04 14:05:55 +0200

    navbar to sfc

--- client/src/components/dashboard/nav-bar/NavBar.jsx
+++ client/src/components/dashboard/nav-bar/NavBar.jsx
@@ -7,112 +7,112 @@ import { Link } from "react-router-dom";
 import "../../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-export default class NavBar extends React.Component {
-  state = {};
-  logOut() {
+
+const NavBar = (props) => {
+  const logOut = () => {
     localStorage.removeItem("token");
     toast.success("You have been logged out!");
     // this.props.history.push("/role");
-  }
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container">
-          <a href="/home" className="navbar-brand">
-            Quiz Platform
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link to={"/questions"} className="nav-link">
-                  Questions
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/categories"} className="nav-link">
-                  Categories
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/results"} className="nav-link">
-                  Results
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/users"} className="nav-link">
-                  Users
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/my-profile"} className="nav-link">
-                  My Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to={"/role"}
-                  className="nav-link"
-                  onClick={() => this.logOut()}
-                >
-                  Log out
-                </Link>
-              </li>
-            </ul>
-          </div>
+  };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <a href="/home" className="navbar-brand">
+          Quiz Platform
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to={"/questions"} className="nav-link">
+                Questions
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/categories"} className="nav-link">
+                Categories
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/results"} className="nav-link">
+                Results
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/users"} className="nav-link">
+                Users
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/my-profile"} className="nav-link">
+                My Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to={"/role"}
+                className="nav-link"
+                onClick={() => this.logOut()}
+              >
+                Log out
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-      // <div>
-      //   <nav className="navbar navbar-expand navbar-dark bg-primary">
-      //     <div className="container">
-      //       <a href="/home" className="navbar-brand">
-      //         Quizapp MERN
-      //       </a>
-      //       {/* <div className="navbar-nav mr-auto"> */}
-      //       <div className="navbar-nav">
-      //         <li className="nav-item">
-      //           <Link to={"/questions"} className="nav-link">
-      //             Questions
-      //           </Link>
-      //         </li>
-      //         <li className="nav-item">
-      //           <Link to={"/categories"} className="nav-link">
-      //             Categories
-      //           </Link>
-      //         </li>
-      //         <li className="nav-item">
-      //           <Link to={"/users"} className="nav-link">
-      //             Users
-      //           </Link>
-      //         </li>
-      //         <li className="nav-item">
-      //           <Link to={"/my-profile"} className="nav-link">
-      //             My Profile
-      //           </Link>
-      //         </li>
-      //         <li className="nav-item">
-      //           <Link
-      //             to={"/role"}
-      //             className="nav-link"
-      //             onClick={() => this.logOut()}
-      //           >
-      //             Log out
-      //           </Link>
-      //         </li>
-      //       </div>
-      //     </div>
-      //   </nav>
-      // </div>
-    );
-  }
-}
+      </div>
+    </nav>
+    // <div>
+    //   <nav className="navbar navbar-expand navbar-dark bg-primary">
+    //     <div className="container">
+    //       <a href="/home" className="navbar-brand">
+    //         Quizapp MERN
+    //       </a>
+    //       {/* <div className="navbar-nav mr-auto"> */}
+    //       <div className="navbar-nav">
+    //         <li className="nav-item">
+    //           <Link to={"/questions"} className="nav-link">
+    //             Questions
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link to={"/categories"} className="nav-link">
+    //             Categories
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link to={"/users"} className="nav-link">
+    //             Users
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link to={"/my-profile"} className="nav-link">
+    //             My Profile
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link
+    //             to={"/role"}
+    //             className="nav-link"
+    //             onClick={() => this.logOut()}
+    //           >
+    //             Log out
+    //           </Link>
+    //         </li>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </div>
+  );
+};
+
+export default NavBar;
